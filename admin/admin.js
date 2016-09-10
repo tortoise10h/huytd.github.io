b62ddbab69251790e8c102118edd073b17ca4a77
Huy Tran <kingbazoka@gmail.com>
2016-09-10 01:13:24 -0700

    admin tool

--- admin/admin.js
+++ admin/admin.js
@@ -2,4 +2,51 @@ let app = angular.module('adminApp', []);
 
 app.controller('admin', function($scope, $http) {
   $scope.publishedPosts = [];
+  $scope.unpublishedPosts = [];
+
+  $scope.refresh = () => {
+    $http.get('/published').success((data) => {
+      $scope.publishedPosts = Array.from(data.published);
+    });
+
+    $http.get('/unpublished').success((data) => {
+      $scope.unpublishedPosts = Array.from(data.unpublished);
+    });
+  }
+
+  $scope.refresh();
+
+  $scope.publishPost = (post, index) => {
+    $scope.unpublishedPosts.splice(index, 1);
+    $scope.publishedPosts.push(post);
+    $scope.updateAll();
+  }
+
+  $scope.unpublishPost = (post, index) => {
+    $scope.publishedPosts.splice(index, 1);
+    $scope.unpublishedPosts.push(post);
+    $scope.updateAll();
+  }
+
+  $scope.updateAll = () => {
+    $http.post('/update', { data: $scope.publishedPosts })
+      .success(function(data) {
+      });
+  }
+
+  $scope.swap = (a, b) => {
+    let temp = $scope.publishedPosts[a];
+    $scope.publishedPosts[a] = $scope.publishedPosts[b];
+    $scope.publishedPosts[b] = temp;
+  }
+
+  $scope.moveUp = (idx) => {
+    $scope.swap(idx, idx-1);
+    $scope.updateAll();
+  }
+
+  $scope.moveDown = (idx) => {
+    $scope.swap(idx, idx+1);
+    $scope.updateAll();
+  }
 });
--- app.js
+++ app.js
@@ -1,11 +1,53 @@
-let express = require('express');
-let app = express();
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser());
 app.use(express.static("admin"));
 
 app.get('/published', function (req, res) {
   let posts = require('./publish.json');
   res.json(posts);
+});
+
+app.post('/update', function(req, res) {
+  let saveData = { published: req.body.data };
+  fs.writeFile('./publish.json', JSON.stringify(saveData), function(err) {
+    res.json({ error: err });
+  });
+});
+
+app.get('/unpublished', function(req, res) {
+  let publishedPosts = Array.from(require('./publish.json').published);
+  let posts = [];
+  fs.readdir(__dirname + '/posts/', function(err, files) {
+    if (err) return;
+    files.forEach(function(f) {
+      if (f.indexOf('.md') != -1) {
+        let postUrl = 'posts/' + f.replace(/\.md/, '.html');
+        let isPublished = publishedPosts.findIndex((e) => {
+          return e.url === postUrl;
+        });
+        if (isPublished == -1) {
+          let fileData = fs.readFileSync(__dirname + '/posts/' + f, { encoding: 'utf8' });
+          if (fileData) {
+            let lines = fileData.split('\n');
+            if (lines.length > 0) {
+              let title = lines[0].replace(/#/g, '').replace("\r\n", '').replace("\n", '').trim();
+              let desc = lines.slice(1).join('').substr(0, 300).replace(/#/g, '') + '...';
+              posts.push({
+                title: title,
+                url: postUrl,
+                desc: desc
+              });
+            }
+          }
+        }
+      }
+    });
+    res.json({ unpublished: posts });
+  });
 });
 
 app.listen(3000);
