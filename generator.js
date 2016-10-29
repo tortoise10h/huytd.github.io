c5b51e66edb73742b857286efd77b3bf3812a75f
Huy Tran <kingbazoka@gmail.com>
2016-10-29 10:53:15 -0700

    Algo

--- generator.js
+++ generator.js
@@ -75,6 +75,58 @@ fs.readdir(__dirname + '/posts/', function(err, files) {
     });
 });
 
+// Generate sub folders
+fs.readdir(__dirname + '/posts/algorithms/', function(err, files) {
+    if (err) return;
+    files.forEach(function(f) {
+        if (f.indexOf('.md') != -1) {
+        	var htmlOutput = __dirname + '/posts/algorithms/' + f.replace('.md', '.html');
+	        var postContent = '';
+	        var htmlContent = '';
+	        var metaData = '';
+	        fs.readFile(__dirname + '/posts/algorithms/' + f, function (err, data) {
+			  if (err)
+			    throw err;
+        console.log("Reading: ", f);
+			  if (data) {
+			  	var markdownPost = data.toString('utf8');
+			  	var lines = markdownPost.split('\n');
+			  	var title = '';
+			  	if (lines.length > 0) {
+			  		title = lines[0].replace(/#/g, '').replace("\r\n", '').replace("\n", '');
+			  		if (lines[lines.length - 6].indexOf('<meta') == 0) {
+			  			metaData = lines.slice(lines.length - 6).join('\n');
+			  			markdownPost = markdownPost.split('\n');
+			  			markdownPost.splice(markdownPost.length - 6);
+			  			markdownPost = markdownPost.join('\n');
+			  		}
+			  	}          
+
+
+        // Custom components
+			  	markdownPost = markdownPost.replace(/<cover>/g, '<div class="cover" style="background-image:url(');
+			  	markdownPost = markdownPost.replace(/<\/cover>/g, '"></div><div class="cover-holder"></div>');
+			  	markdownPost = markdownPost.replace(/<math>/g, '<pre class="math">$$');
+			  	markdownPost = markdownPost.replace(/<\/math>/g, '$$</pre>');
+
+			  	postContent = marked(markdownPost);
+			  	htmlContent = templateHtml.replace('{%content%}', postContent);
+			  	htmlContent = htmlContent.replace('{%title%}', title);
+			  	htmlContent = htmlContent.replace('{%meta%}', metaData);
+			  	htmlContent = htmlContent.replace('{%posturl%}', 'http://huytd.github.io/posts/' + f.replace('.md', '.html'));
+
+			  	fs.writeFile(htmlOutput, htmlContent, function (err) {
+				     if (err)
+				       throw err;
+				     else
+				     	console.log('>>', htmlOutput);
+				});
+			  }
+			});
+        }
+    });
+});
+
 console.log('Generating index page...');
 var publishedPosts = Array.from(require('./publish.json').published);
 var htmlOutput = __dirname + '/index.html';
