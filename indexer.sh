ead7a53203f8892c2220284b6982a4a4bb44cbc6
Huy Tran <kingbazoka@gmail.com>
2020-02-08 18:38:22 -0800

    Auto publishing

--- indexer.sh
+++ indexer.sh
@@ -1,4 +1,4 @@
-echo "<link href='_css/style.css' rel='stylesheet'/><div id='content'><h1>Huy's Notes</h1><ul>"
+echo "<link href='_css/style.css' rel='stylesheet'/><div id='content homepage'><h1>$ ls /home/huy/notes/</h1><ul>"
 for file in *.html
 do
   if [[ $file =~ .*index.html.* ]]; then
